e04c5ca0f2b7fecdcef847bf384f3502e3dd0350
ddryden14 <35632618+ddryden14@users.noreply.github.com>
2018-08-28 16:15:58 -0500

    Added Lights Out Visual Studio Project

---
+++ LightsOut/LightsOut/AboutForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace LightsOut
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.authorLabel = new System.Windows.Forms.Label();
+            this.goalLabel = new System.Windows.Forms.Label();
+            this.okButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.Red;
+            this.titleLabel.Location = new System.Drawing.Point(108, 26);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(116, 25);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Lights Out!";
+            // 
+            // authorLabel
+            // 
+            this.authorLabel.AutoSize = true;
+            this.authorLabel.Location = new System.Drawing.Point(97, 75);
+            this.authorLabel.Name = "authorLabel";
+            this.authorLabel.Size = new System.Drawing.Size(85, 13);
+            this.authorLabel.TabIndex = 1;
+            this.authorLabel.Text = "By Isaac Dryden";
+            // 
+            // goalLabel
+            // 
+            this.goalLabel.AutoSize = true;
+            this.goalLabel.Location = new System.Drawing.Point(48, 103);
+            this.goalLabel.Name = "goalLabel";
+            this.goalLabel.Size = new System.Drawing.Size(183, 13);
+            this.goalLabel.TabIndex = 2;
+            this.goalLabel.Text = "Turn all the lights out to win the game";
+            // 
+            // okButton
+            // 
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(100, 136);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 3;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LightsOut.Properties.Resources.light_bulb_icon;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(51, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 171);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.goalLabel);
+            this.Controls.Add(this.authorLabel);
+            this.Controls.Add(this.titleLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "About";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label authorLabel;
+        private System.Windows.Forms.Label goalLabel;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ LightsOut/LightsOut/MainForm.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LightsOut
+{
+    public partial class MainForm : Form
+    {
+        private const int GRIDOFFSET = 25;
+        private int gridLength = 200;
+        private int numCells = 3;
+        private int cellLength = 0;
+
+        private bool[,] grid;
+        private Random random;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            cellLength = gridLength / numCells;
+
+            random = new Random();
+            grid = new bool[numCells, numCells];
+
+            for (int row = 0; row < numCells; row++)
+            {
+                for (int column = 0; column < numCells; column++)
+                {
+                    grid[row, column] = true;
+                }
+            }
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            for (int row = 0; row < numCells; row++)
+            {
+                for (int column = 0; column < numCells; column++)
+                {
+                    Brush brush;
+                    Pen pen;
+
+                    if(grid[row, column])
+                    {
+                        pen = Pens.White;
+                        brush = Brushes.ForestGreen;
+                    }
+                    else
+                    {
+                        pen = Pens.White;
+                        brush = Brushes.Black;
+                    }
+
+                    int x = column * cellLength + GRIDOFFSET;
+                    int y = row * cellLength + GRIDOFFSET;
+
+                    g.DrawRectangle(pen, x, y, cellLength, cellLength);
+                    g.FillRectangle(brush, x + 1, y + 1, cellLength - 1, cellLength - 1);
+                }
+            }
+        }
+
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.X < GRIDOFFSET || e.X > cellLength * numCells + GRIDOFFSET ||
+                e.Y < GRIDOFFSET || e.Y > cellLength * numCells + GRIDOFFSET)
+            {
+                return;
+            }
+
+            int row = (e.Y - GRIDOFFSET) / cellLength;
+            int column = (e.X - GRIDOFFSET) / cellLength;
+
+            for(int i = row-1; i <= row+1; i++)
+            {
+                for(int j = column-1; j <= column+1; j++)
+                {
+                    if(i >= 0 && i < numCells && j >= 0 && j < numCells)
+                    {
+                        grid[i, j] = !grid[i, j];
+                    }
+                }
+            }
+
+            this.Invalidate();
+
+            if(PlayerWon())
+            {
+                MessageBox.Show(this, "Congratulations!, You've won!", "Lights Out!", 
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private bool PlayerWon()
+        {
+            bool result = true;
+
+            for (int row = 0; row < numCells; row++)
+            {
+                for (int column = 0; column < numCells; column++)
+                {
+                    if (result && grid[row, column])
+                    {
+                        result = false;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            for (int row = 0; row < numCells; row++)
+            {
+                for (int column = 0; column < numCells; column++)
+                {
+                    grid[row, column] = random.Next(2) == 1;
+                }
+            }
+
+            this.Invalidate();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            newGameButton_Click(sender, e);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutForm aboutForm = new AboutForm();
+            aboutForm.ShowDialog(this);
+        }
+    }
+}
